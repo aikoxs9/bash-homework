166f198e7c3622c16c782714a2026b15f562a106
aikoxs9 <ainuskamursabek@gmail.com>
2025-06-04 21:25:42 +0000

    Add Homework-12 with introduce.sh and input.sh

---
+++ homework-12/input.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+read -p "Enter your name: " name
+read -p "Enter your age: " age
+read -p "Enter your fav language: " fav_language 
+read -p "Enter your hobby: " hobby
+read -p "Enter your dream: " dream 
+
+echo "Hello, my name is $name. I'm $age years old. I love my country and culture. That's why $name her $fav_language. Since childhood my hobby is $hobby. In future I want to work in an IT company and for this I am studyng to be $dream."

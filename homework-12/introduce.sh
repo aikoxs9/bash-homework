93da5a794e38d9e44fe9c4a212e260f5b454b83e
aikoxs9 <ainuskamursabek@gmail.com>
2025-06-05 07:08:17 +0000

    homework.sh, k-pop, wordpress, binary functions

--- homework-12/introduce.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-name="Ainuska"
-age=22
-fav_language="Kyrgyz"
-hobby="dancing"
-dream="a Devops engineer"
-
-echo "Hello, my name is $name, and I'm $age years old.I love my country and culture. That's why $name her $fav_language. Since childhood my hobby is $hobby. In future I want to work in an IT company andfor this I am studying to be $dream." 

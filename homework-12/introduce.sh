4992f44be25fd8d83d92b15a1b94206bad22bd34
aikoxs9 <ainuskamursabek@gmail.com>
2025-06-08 03:59:11 +0000

    introduce input

---
+++ homework-12/introduce.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+name="Ainuska"
+age=22
+fav_language="Python"
+hobby="reading tech blogs"
+dream="a Devops engineer"
+
+echo "Hi, my name is $name, and I'm $age years old. Every morning, $name likes to spend time $hobby.
+As a $age-year-old, I believe it's never too late to follow your dreams. My favorite programming language is $favorite_language.
+I code in $favorite_language almost every day!
+One day, I hope my love for $favorite_language will help me achieve my dream of $dream.
+When I'm not busy working toward $dream, you'll find me $hobby to relax.
+That's a little about me, $name — a $age-year-old who loves $favorite_language, enjoys $hobby, and dreams of $dream!"
